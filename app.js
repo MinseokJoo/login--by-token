0217d1825be5c9a5b1f5582a21b0f3af0b73d0b3
Joominseok <jumseok12@gmail.com>
2022-12-31 11:28:03 +0900

    숙제 제출, sessionObj 설정값은 외우지 못했음...ㅠ secret리아 store은 외웠는데...ㅜ

--- app.js
+++ app.js
@@ -1,20 +1,50 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
+const session = require("express-session")
+const store = require("memorystore")(session)
 
 const app = express()
 
 app.use(express.json(), cookieParser())
 
-app.get("/users", (req,res) => {
-  const userId = req.cookies.userId
+const sessionObj = {
+  secret: "minseok",
+  store: new store(),
+  resave: false,
+  saveUninitialized: true
+}
 
-  console.log(userId)
-  res.send("users page")
+app.use(session(sessionObj))
+
+const users = [
+  {id: "noggong"},
+  {id: "hyowon"},
+  {id: "kimin"}
+]
+
+app.get("/users", (req,res) => {
+  const userId = req.session.userId
+
+  const user = users.find(user => userId === user.id)
+
+  if(!user) {
+    return res.status(401).json({message: "로그인 이후에 사용 가능한 기능입니다."})
+  }
+
+  res.send(user)
 })
 
 app.post("/login", (req,res) => {
   const {userId} = req.body
-  res.send("login page")
+
+  const user = users.find(user => user.id === userId)
+
+  if(!user) {
+    return res.status(401).json({message: "없는 아이디입니다아아아아아아"})
+  }
+
+  req.session.userId = user.id
+  res.send(user.id)
 })
 
 app.post("/logout", (req,res) => {
